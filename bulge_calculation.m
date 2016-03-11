a1ab50f1403cece056e8ece43c9667b9ad491fdb
somkheart <engineer_robot@hotmail.com>
2016-03-11 12:11:14 +0700

    update ส่วนของ GIPGUI

--- bulge_calculation.m
+++ bulge_calculation.m
@@ -1,15 +1,27 @@
+ 
+%Initial Array For Testing
+ X_CAD=[1 2 3 4 5 6];
+ Y_CAD=[1 2 3 4 5 6];
+ 
+ X_IFS=[1 2 3 4 5 6];
+ Y_IFS=[1 2 3 4 5 6];
+ 
+ i=0;
+ 
 % Extrack the end points
-P1=[X_CAD(i) Y_CAD(i) ];
+P1=[X_CAD(i) Y_CAD(i)];
 P2=[X_CAD(i+1) Y_CAD(i+1)];
 
 %Calculate nornal vector (with right-hand-rule  convention)
-N=cross([P2-P1 0],[0 0 -1]); N=N(1:2); N=N/norm(N);
+N=cross([P2-P1 0],[0 0 -1]);
+N=N(1:2); 
+N=N/norm(N);
 
 %Calculate chord length (P1 to P2)
 chord = norm(P1-P2);
 
 %Check if there is bulge
-if(BU_CAD(i) ==0)% no bulge,meaning straight line
+if(BU_CAD(i) == 0)% no bulge,meaning straight line
     % Subdivide the line
     np=ceil(chord/ds)+1;
     % np=2;
